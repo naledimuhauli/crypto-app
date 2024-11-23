03f976432c3415c09f70722d54ffba371e3aa5b6
siphokazi <naledimuhauli@gmail.com>
2024-11-23 05:23:08 +0200

    styled the navbar

--- client/src/components/chatWindow.js
+++ client/src/components/chatWindow.js
@@ -4,6 +4,10 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import Sidebar from './sidebar';
+import notification from '../images/notification.png';
+import help from '../images/help.png';
+import userImage from '../images/Avatar.png';
+import arrow from '../images/arrow-down.png';
 
 
 function ChatWindow() {
@@ -15,10 +19,34 @@ function ChatWindow() {
                 <Sidebar />
             </div>
             <div className="main-chat">
-                <div className="navbar">
-                    <p className="name">Siphokazi</p>
-                    <hr />
-                </div>
+            <div className="navbar d-flex align-items-center justify-content-between">
+    <form className="search-form" role="search">
+        <input
+            type="search"
+            className="form-control"
+            placeholder="Search type of keywords"
+            aria-label="Search"
+        />
+    </form>
+    <div className="icons d-flex">
+        <img src={notification} alt="notification" className="icon" />
+        <img src={help} alt="help" className="icon ms-3" />
+    </div>
+    <div className="user-details d-flex align-items-center">
+        <img
+            src={userImage}
+            alt="user"
+            className="user-image rounded-circle"
+        />
+        <div className="ms-3">
+            <p className="name mb-0">Siphokazi</p>
+            <p className="email text-muted">siphokazi@example.com</p>
+        </div>
+        <img src={arrow} alt='arrow-down' className='arrow'/>
+    </div>
+    <hr className="navbar-line" />
+</div>
+
                 </div>
         </div>
     );
