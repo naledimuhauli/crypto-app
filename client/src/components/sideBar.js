eb0ad2e5cf18ead64b8f3e2cafef9c4506fae5d8
siphokazi <naledimuhauli@gmail.com>
2024-11-23 03:05:12 +0200

    styled the sidebar

--- client/src/components/sideBar.js
+++ client/src/components/sideBar.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './style.css';
+import logo from '../images/Logo.png';
+import chart from '../images/graph.png';
+import overview from '../images/element-3.png';
+import transactions from '../images/wallet-minus.png';
+import wallet from '../images/wallet-2.png';
+import setting from '../images/setting-2.png';
+import mailbox from '../images/sms.png';
+import logout from '../images/logout.png';
+import ellipse from '../images/Ellipse.png';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h2 className="headig mb-5">
-               Crypx Superpage
+            <h2 className="heading mb-5">
+              <img src={logo} alt='logo' className='logo' />
+               CrypX
             </h2>
             <ul>
-                <li className='active'>AI chat</li>
-                <li> Members</li>
-                <li> Integrations</li>
-                <li> Refer Friends</li>
-                <li> Pricing Plans</li>
-                <li> Settings</li>
+                <li className='active'> <img src={overview} alt='logo' /> Overview 
+                <img src={ellipse} alt='logo' className='ellipse'/>
+                </li>
+                <li> <img src={chart} alt='logo' /> Chart</li>
+                <li> <img src={transactions} alt='logo' /> Transactions</li>
+                <li> <img src={wallet} alt='logo' /> Wallet</li>
+                <li> <img src={mailbox} alt='logo' /> Mail Box</li>
+                <li> <img src={setting} alt='logo' /> Setting</li>
+                <li> <img src={logout} alt='logo' /> Logout</li>
             </ul>
         </div>
     );
