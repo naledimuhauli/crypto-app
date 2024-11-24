ff41e47c429f46621342f239b304f01f4e52e86e
siphokazi <naledimuhauli@gmail.com>
2024-11-25 01:58:59 +0200

    fixed server.js and installed axios

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const axios = require('axios'); // Add axios import
 require('dotenv').config();
 const authRoutes = require('./routes/auth');
 
@@ -28,6 +29,7 @@ db.connect((err) => {
 // Use the routes
 app.use('/auth', authRoutes(db));
 
+// Fetch BTC data
 const fetchBTCData = async () => {
     try {
       const response = await axios.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart', {
@@ -41,19 +43,17 @@ const fetchBTCData = async () => {
       console.error('Error fetching BTC data:', error);
       return [];
     }
-  };
+};
 
-  const prepareChartData = (prices) => {
+const prepareChartData = (prices) => {
     const labels = prices.map(([timestamp]) => new Date(timestamp).toLocaleDateString());
     const dataPoints = prices.map(([, price]) => price);
-  
+
     return {
       labels, // Dates
       dataPoints, // Prices
     };
-  };
-  
-
+};
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
